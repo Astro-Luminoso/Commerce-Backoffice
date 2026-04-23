173d214b15853563a22a0c3e5c3b07b7fda77f6a
hanbyeol.yang <beuri97@gmail.com>
2026-04-23 15:14:22 +0900

    feat: initial commit

---
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/CommerceBackofficeApplication.java
@@ -0,0 +1,13 @@
+package dev.nbcsparta.assignment.commerce_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceBackofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceBackofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/nbcsparta/assignment/commerce_backoffice/CommerceBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nbcsparta.assignment.commerce_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceBackofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
