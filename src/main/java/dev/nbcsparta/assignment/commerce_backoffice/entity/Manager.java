062eb42cbfb43166a6fd201e5b32e13a5b7d65f8
hanbyeol.yang <beuri97@gmail.com>
2026-04-24 15:15:22 +0900

    chore: Manager 엔티티 생성
    - Enum Role, AccountStatus 생성

---
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/entity/Manager.java
@@ -0,0 +1,58 @@
+package dev.nbcsparta.assignment.commerce_backoffice.entity;
+
+import dev.nbcsparta.assignment.commerce_backoffice.enumerate.AccountStatus;
+import dev.nbcsparta.assignment.commerce_backoffice.enumerate.Role;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Manager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private AccountStatus status;
+
+    @Column(nullable = false)
+    private Role role;
+
+    @CreatedDate
+    private LocalDateTime registrationDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+    protected Manager() {
+    }
+
+    public Manager(String name, String email, String password, String phoneNumber, AccountStatus status) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.status = status;
+    }
+
+    public Manager(String name, String email, String password, String phoneNumber) {
+        this(name, email, password, phoneNumber, AccountStatus.PENDING);
+    }
+}
---
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/enumerate/Role.java
@@ -0,0 +1,19 @@
+package dev.nbcsparta.assignment.commerce_backoffice.enumerate;
+
+public enum Role {
+    CS_MANAGER("CS_Manager", 1),
+    Ops_MANAGER("Ops_Manager", 2),
+    Super_MANAGER("Super_Manager", 3);
+
+    private final String roleName;
+    private int accessLevel;
+
+    Role(String roleName, int accessLevel) {
+        this.roleName = roleName;
+        this.accessLevel = accessLevel;
+    }
+
+    public int getAccessLevel() {
+        return this.accessLevel;
+    }
+}
