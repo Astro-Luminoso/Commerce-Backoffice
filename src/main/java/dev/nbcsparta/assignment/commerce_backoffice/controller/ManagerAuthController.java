23fc62c8b712b5dae240a0677c71af1fa72856f3
kolyn092 <kolyn092@gmail.com>
2026-04-24 21:01:29 +0900

    feat: 로그인 기능 구현

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/controller/ManagerAuthController.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/controller/ManagerAuthController.java
@@ -2,7 +2,13 @@ package dev.nbcsparta.assignment.commerce_backoffice.controller;
 
 import dev.nbcsparta.assignment.commerce_backoffice.dto.CreateManagerRequest;
 import dev.nbcsparta.assignment.commerce_backoffice.dto.CreateManagerResponse;
+import dev.nbcsparta.assignment.commerce_backoffice.dto.LoginRequest;
+import dev.nbcsparta.assignment.commerce_backoffice.dto.SessionManager;
 import dev.nbcsparta.assignment.commerce_backoffice.service.ManagerAuthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,5 +29,14 @@ public class ManagerAuthController {
     public ResponseEntity<CreateManagerResponse> register(@Valid @RequestBody CreateManagerRequest req) {
         CreateManagerResponse res = managerAuthService.register(req);
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request, HttpServletRequest httpRequest) {
+        SessionManager sessionManager = managerAuthService.login(request);
+        HttpSession session = httpRequest.getSession(true);
+        session.setAttribute("LOGIN_MANAGER", sessionManager);
+        session.setMaxInactiveInterval(60 * 60 * 24);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/service/ManagerAuthService.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/service/ManagerAuthService.java
@@ -3,8 +3,13 @@ package dev.nbcsparta.assignment.commerce_backoffice.service;
 import dev.nbcsparta.assignment.commerce_backoffice.config.PasswordEncoder;
 import dev.nbcsparta.assignment.commerce_backoffice.dto.CreateManagerRequest;
 import dev.nbcsparta.assignment.commerce_backoffice.dto.CreateManagerResponse;
+import dev.nbcsparta.assignment.commerce_backoffice.dto.LoginRequest;
+import dev.nbcsparta.assignment.commerce_backoffice.dto.SessionManager;
 import dev.nbcsparta.assignment.commerce_backoffice.entity.Manager;
+import dev.nbcsparta.assignment.commerce_backoffice.enumerate.AccountStatus;
 import dev.nbcsparta.assignment.commerce_backoffice.exception.ConflictUserException;
+import dev.nbcsparta.assignment.commerce_backoffice.exception.LoginNotAllowedException;
+import dev.nbcsparta.assignment.commerce_backoffice.exception.NotMatchException;
 import dev.nbcsparta.assignment.commerce_backoffice.repository.ManagerAuthRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,6 +36,28 @@ public class ManagerAuthService {
         Manager resManager = managerAuthRepository.save(manager);
 
         return CreateManagerResponse.from(resManager);
+    }
+
+    @Transactional
+    public SessionManager login(LoginRequest req) {
+        Manager manager = managerAuthRepository.findByEmail(req.email())
+                .orElseThrow(() -> new NotMatchException("이메일 또는 비밀번호가 일치하지 않습니다."));
+
+        boolean isMatch = passwordEncoder.matches(req.password(), manager.getPassword());
+        if (!isMatch) {
+            throw new NotMatchException("이메일 또는 비밀번호가 일치하지 않습니다.");
+        }
+
+        switch (manager.getStatus()) {
+            case ACTIVE -> {
+                return SessionManager.from(manager);
+            }
+            case INACTIVE -> throw new LoginNotAllowedException("계정 비활성화 상태입니다.");
+            case SUSPENDED -> throw new LoginNotAllowedException("계정이 정지된 상태입니다.");
+            case PENDING -> throw new LoginNotAllowedException("계정 승인대기 중입니다.");
+            case DENIED -> throw new LoginNotAllowedException("계정 신청이 거부된 상태입니다.");
+            default -> throw new IllegalStateException("유효하지 않은 상태 값: " + manager.getStatus());
+        }
     }
 
 }
