b2d4fa5f12f1055f40f66dec77b239a3e098cdd8
kolyn092 <kolyn092@gmail.com>
2026-04-24 16:59:25 +0900

    feat: register 구현

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/controller/ManagerAuthController.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/controller/ManagerAuthController.java
@@ -1,6 +1,13 @@
 package dev.nbcsparta.assignment.commerce_backoffice.controller;
 
+import dev.nbcsparta.assignment.commerce_backoffice.dto.request.CreateManagerRequest;
+import dev.nbcsparta.assignment.commerce_backoffice.dto.response.CreateManagerResponse;
 import dev.nbcsparta.assignment.commerce_backoffice.service.ManagerAuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +17,11 @@ public class ManagerAuthController {
 
     public ManagerAuthController(ManagerAuthService managerAuthService) {
         this.managerAuthService = managerAuthService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<CreateManagerResponse> register(@Valid @RequestBody CreateManagerRequest req) {
+        CreateManagerResponse res = managerAuthService.register(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
     }
 }
--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/repository/ManagerAuthRepository.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/repository/ManagerAuthRepository.java
@@ -4,4 +4,5 @@ import dev.nbcsparta.assignment.commerce_backoffice.entity.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ManagerAuthRepository extends JpaRepository<Manager, Long> {
+    boolean existsByEmail(String email);
 }
