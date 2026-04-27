c5802d4da8b245d4581b7d9f1b982eaf8dc5f26b
kolyn092 <kolyn092@gmail.com>
2026-04-27 14:41:09 +0900

    feat: Bean Validation 예외 핸들링

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package dev.nbcsparta.assignment.commerce_backoffice.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +15,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(ex.getStatus())
                 .body(ex.getMessage());
+    }
+
+    // Bean Validation 예외 핸들링
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse("입력 값이 올바르지 않습니다.");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
     }
 }
