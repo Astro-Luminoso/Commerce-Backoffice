213b7bc6717a2988174f59633f120004fe774a9c
kolyn092 <kolyn092@gmail.com>
2026-04-29 01:17:29 +0900

    feat: 리뷰 대시보드용 쿼리 조회 추가

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/repository/ReviewRepository.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/repository/ReviewRepository.java
@@ -1,7 +1,15 @@
 package dev.nbcsparta.assignment.commerce_backoffice.repository;
 
+import dev.nbcsparta.assignment.commerce_backoffice.dto.dashboard.data.ReviewDashboard;
 import dev.nbcsparta.assignment.commerce_backoffice.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("SELECT new dev.nbcsparta.assignment.commerce_backoffice.dto.dashboard.data.ReviewDashboard(" +
+            "COUNT(r)," +
+            "COALESCE(AVG(r.rating), 0))" +
+            "FROM Review r")
+    ReviewDashboard getStatistics();
 }
--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/service/ReviewService.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/service/ReviewService.java
@@ -1,7 +1,9 @@
 package dev.nbcsparta.assignment.commerce_backoffice.service;
 
+import dev.nbcsparta.assignment.commerce_backoffice.dto.dashboard.data.ReviewDashboard;
 import dev.nbcsparta.assignment.commerce_backoffice.repository.ReviewRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReviewService {
@@ -12,5 +14,8 @@ public class ReviewService {
         this.reviewRepository = reviewRepository;
     }
 
-
+    @Transactional
+    public ReviewDashboard getStatistics() {
+        return reviewRepository.getStatistics();
+    }
 }
