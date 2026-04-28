b85acd0d8dc61ca331f063b59784668fc07f8b07
hanbyeol.yang <beuri97@gmail.com>
2026-04-29 03:33:44 +0900

    feat: add new services and repository for new entity Authority and HasAuthority

---
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/repository/AuthorityRepository.java
@@ -0,0 +1,8 @@
+package dev.nbcsparta.assignment.commerce_backoffice.repository;
+
+import dev.nbcsparta.assignment.commerce_backoffice.entity.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+
+}
