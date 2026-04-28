d255db41dd5788582545d83dcfbf8d811395bfd6
kolyn092 <kolyn092@gmail.com>
2026-04-29 00:29:36 +0900

    feat: 대시보드용 쿼리 조회 추가

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/repository/ManagerRepository.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/repository/ManagerRepository.java
@@ -26,8 +26,9 @@ public interface ManagerRepository extends JpaRepository<Manager, Long> {
             @Param("status")AccountStatus status,
             Pageable pageable);
 
-    //@Query("""
-    //        SELECT new dev.nbcsparta.assignment.commerce_backoffice.dto.dashboard.data.ManagerDashboard(
-    //        COUNT(m), SUM(CASE WHEN m.status = dev.nbcsparta.assignment.commerce_backoffice.enumerate.AccountStatus.ACTIVE THEN 1 ELSE 0) AS sum FROM Manager m""")
-    //ManagerDashboard getStatistics();
+    @Query("SELECT new dev.nbcsparta.assignment.commerce_backoffice.dto.dashboard.data.ManagerDashboard(" +
+            "count(m)," +
+            "sum(CASE WHEN m.status = dev.nbcsparta.assignment.commerce_backoffice.enumerate.AccountStatus.ACTIVE THEN 1 ELSE 0 END))" +
+            "FROM Manager m")
+    ManagerDashboard getStatistics();
 }
