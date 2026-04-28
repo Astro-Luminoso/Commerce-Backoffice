47d5b3204337e4bc3c7387bf5925faeaa592a552
kolyn092 <kolyn092@gmail.com>
2026-04-28 23:45:14 +0900

    feat: 대시보드 조회 controller 함수 추가

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/controller/DashboardController.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/controller/DashboardController.java
@@ -1,7 +1,12 @@
 package dev.nbcsparta.assignment.commerce_backoffice.controller;
 
 import dev.nbcsparta.assignment.commerce_backoffice.config.Authentication;
+import dev.nbcsparta.assignment.commerce_backoffice.dto.CommonResponse;
+import dev.nbcsparta.assignment.commerce_backoffice.dto.dashboard.DashboardResponse;
 import dev.nbcsparta.assignment.commerce_backoffice.service.DashboardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +18,15 @@ public class DashboardController {
     public DashboardController(DashboardService dashboardService, Authentication authentication) {
         this.dashboardService = dashboardService;
         this.authentication = authentication;
+    }
+
+    @GetMapping("/dashboard")
+    public ResponseEntity<CommonResponse<DashboardResponse>> getDashboard() {
+        authentication.getCurrentManager();
+        DashboardResponse res = dashboardService.getDashboard();
+
+        return CommonResponse
+                .success(HttpStatus.OK, "대시보드 조회 성공", res)
+                .toResponseEntity();
     }
 }
