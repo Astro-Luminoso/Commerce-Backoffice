6c3b1e0ef6f4bd2a710f2af0202023d127ee50e2
tjdgns0618 <gruddufs@gmail.com>
2026-04-28 21:39:00 +0900

    refactor : 원래 넣었던 Formula는 N+1 문제가 발생할 수 있기 때문에 DTO에 담아서 주는 쿼리문 작성 - CustomerRepository에서 customer와 order를 left join 하여 그룹화하여서 count와 sum을 이용하여 총 주문 수, 총 주문 가격을 담아서 객체로 만들었습니다. - 객체로 생성해서 돌려주기때문에 전체 조회와 단 건 조회 쿼리 메서드가 따로 있습니다. - 총 주문 수, 총 주문 가격은 Long 타입으로 받도록 하였습니다.

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/entity/Customer.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/entity/Customer.java
@@ -3,7 +3,6 @@ package dev.nbcsparta.assignment.commerce_backoffice.entity;
 import dev.nbcsparta.assignment.commerce_backoffice.dto.CreateCustomerRequest;
 import dev.nbcsparta.assignment.commerce_backoffice.enumerate.AccountStatus;
 import jakarta.persistence.*;
-import org.hibernate.annotations.Formula;
 
 @Entity
 @Table(name = "customers")
@@ -12,12 +11,6 @@ public class Customer extends User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Formula("(SELECT COUNT(*) FROM orders o WHERE o.customer_id = id)")
-    private Integer totalOrderCount;
-
-    @Formula("(SELECT COALESCE(SUM(o.total_price), 0) FROM orders o WHERE o.customer_id = id)")
-    private Integer totalOrderPrice;
 
     protected Customer() {
         super();
@@ -38,13 +31,5 @@ public class Customer extends User {
 
     public Long getId() {
         return id;
-    }
-
-    public Integer getTotalOrderCount() {
-        return totalOrderCount != null ? totalOrderCount : 0;
-    }
-
-    public Integer getTotalOrderPrice() {
-        return totalOrderPrice != null ? totalOrderPrice : 0;
     }
 }
--- src/test/java/dev/nbcsparta/assignment/commerce_backoffice/repository/CustomerRepositoryTest.java
+++ src/test/java/dev/nbcsparta/assignment/commerce_backoffice/repository/CustomerRepositoryTest.java
@@ -1,6 +1,7 @@
 package dev.nbcsparta.assignment.commerce_backoffice.repository;
 
 import dev.nbcsparta.assignment.commerce_backoffice.dto.CreateCustomerRequest;
+import dev.nbcsparta.assignment.commerce_backoffice.dto.CustomerDetail;
 import dev.nbcsparta.assignment.commerce_backoffice.dto.GetCustomerPageFilter;
 import dev.nbcsparta.assignment.commerce_backoffice.dto.UpdateMyProfileRequest;
 import dev.nbcsparta.assignment.commerce_backoffice.entity.Customer;
@@ -67,10 +68,10 @@ class CustomerRepositoryTest {
         Pageable pageable = PageRequest.of(0, 3);
         // 이메일에 4가 들어가는 사람 필터링
         GetCustomerPageFilter filter = new GetCustomerPageFilter(null, "4", AccountStatus.ACTIVE);
-        Page<Customer> result = customerRepository.findAllByFilters(filter, pageable);
+        Page<CustomerDetail> result = customerRepository.findAllCustomerByFilters(filter, pageable);
         // 1사람만 나옴, 이름은 홍길남
         assertEquals(1, result.getContent().size());
-        assertEquals("홍길남", result.getContent().getFirst().getName());
+        assertEquals("홍길남", result.getContent().getFirst().name());
     }
 
     @Test
@@ -107,9 +108,9 @@ class CustomerRepositoryTest {
 
         Pageable pageable = PageRequest.of(0, 10);
         GetCustomerPageFilter filter = new GetCustomerPageFilter(null, "3", AccountStatus.INACTIVE);
-        Page<Customer> result = customerRepository.findAllByFilters(filter, pageable);
+        Page<CustomerDetail> result = customerRepository.findAllCustomerByFilters(filter, pageable);
 
-        assertEquals("홍갈동", result.getContent().getFirst().getName());
+        assertEquals("홍갈동", result.getContent().getFirst().name());
     }
 
     @Test
