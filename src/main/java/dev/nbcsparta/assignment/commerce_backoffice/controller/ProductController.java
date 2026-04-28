f185718b181ec527e05065ba2e2c464d978fbd6a
jy030506@naver.com <jy030506@naver.com>
2026-04-28 17:03:27 +0900

    refactor: 상품 컨트롤러에서 좀 더 명확한 응답 메시지를 전달하도록 수정

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/controller/ProductController.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/controller/ProductController.java
@@ -58,7 +58,7 @@ public class ProductController {
         GetListProductResponse<GetPageProductResponse> response = productService.getAllProduct(name, category, status, pageable);
 
         return CommonResponse
-                .success(HttpStatus.OK, "상품 페이지 조회 완료", response)
+                .success(HttpStatus.OK, "상품 목록 조회 성공", response)
                 .toResponseEntity();
     }
 
