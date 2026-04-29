1788bfe4142aad8a02d7d6cd87c7d61fc5c2627d
jy030506@naver.com <jy030506@naver.com>
2026-04-29 16:15:41 +0900

    hotfix: ReviewNotFoundException 에서 전달하는 상태코드 변경
    BAD_REQUEST -> NOT_FOUND

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/exception/ReviewNotFoundException.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/exception/ReviewNotFoundException.java
@@ -3,5 +3,5 @@ package dev.nbcsparta.assignment.commerce_backoffice.exception;
 import org.springframework.http.HttpStatus;
 
 public class ReviewNotFoundException extends ServiceException {
-    public ReviewNotFoundException() { super(HttpStatus.BAD_REQUEST, "존재하지 않는 리뷰입니다."); }
+    public ReviewNotFoundException() { super(HttpStatus.NOT_FOUND, "존재하지 않는 리뷰입니다."); }
 }
