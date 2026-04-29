b2256cb8a7d314545736ea66d6400ccdabedd418
tjdgns0618 <gruddufs@gmail.com>
2026-04-29 20:33:39 +0900

    refactor : 동일한 예외 발생 조건 통합

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/service/OrderService.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/service/OrderService.java
@@ -8,7 +8,6 @@ import dev.nbcsparta.assignment.commerce_backoffice.entity.Manager;
 import dev.nbcsparta.assignment.commerce_backoffice.entity.Order;
 import dev.nbcsparta.assignment.commerce_backoffice.entity.Product;
 import dev.nbcsparta.assignment.commerce_backoffice.enumerate.ProductStatus;
-import dev.nbcsparta.assignment.commerce_backoffice.exception.DiscontinuedProductException;
 import dev.nbcsparta.assignment.commerce_backoffice.exception.OrderNotFoundException;
 import dev.nbcsparta.assignment.commerce_backoffice.exception.OutOfStockException;
 import dev.nbcsparta.assignment.commerce_backoffice.repository.OrderRepository;
@@ -50,12 +49,8 @@ public class OrderService {
     public OrderDetail createOrder(CreateOrderRequest request, Long managerId) {
         Product product = productService.getProductById(request.productId());
 
-        if (product.getStatus() == ProductStatus.SOLD_OUT) {
+        if (product.getStatus() == ProductStatus.SOLD_OUT && product.getStatus() == ProductStatus.DISCONTINUED) {
             throw new OutOfStockException();
-        }
-
-        if (product.getStatus() == ProductStatus.DISCONTINUED) {
-            throw new DiscontinuedProductException();
         }
 
         Customer customer = customerService.getCustomerById(request.customerId());
