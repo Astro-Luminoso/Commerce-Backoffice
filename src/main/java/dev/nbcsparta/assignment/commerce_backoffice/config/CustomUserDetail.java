9c2c668c91b1018c97fd9565814efcb3a91615a1
hanbyeol.yang <beuri97@gmail.com>
2026-04-30 15:25:36 +0900

    feat: CustomUserDetail 업데이트
    - 유효 토큰 uuid 및 토큰 만료기간 필드 추가

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/config/CustomUserDetail.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/config/CustomUserDetail.java
@@ -4,21 +4,37 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CustomUserDetail extends User {
 
     private final Long managerId;
+    private final String jwtUuid;
+    private final Date exp;
 
     public CustomUserDetail(
             String username,
             Long managerId,
-            Collection<? extends GrantedAuthority> authorities
+            String jwtUuid,
+            Collection<? extends GrantedAuthority> authorities,
+            Date exp
     ) {
         super(username, "", authorities);
         this.managerId = managerId;
+        this.jwtUuid = jwtUuid;
+        this.exp = exp;
+
     }
 
     public long getManagerId() {
         return this.managerId;
+    }
+
+    public Map<String, Date> getBlackListDetail() {
+        Map<String, Date> detail = new ConcurrentHashMap<>();
+        detail.put(jwtUuid, exp);
+        return detail;
     }
 }
