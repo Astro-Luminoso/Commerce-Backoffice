3a595ae1ae427530e0d683a711f7fc6a6e5681d6
kolyn092 <kolyn092@gmail.com>
2026-04-30 12:57:46 +0900

    fix: manager validation 수정 및 message 추가

--- src/main/java/dev/nbcsparta/assignment/commerce_backoffice/dto/manager/ManagerStatusUpdate.java
+++ src/main/java/dev/nbcsparta/assignment/commerce_backoffice/dto/manager/ManagerStatusUpdate.java
@@ -4,7 +4,7 @@ import dev.nbcsparta.assignment.commerce_backoffice.enumerate.AccountStatus;
 import jakarta.validation.constraints.NotNull;
 
 public record ManagerStatusUpdate(
-        @NotNull
+        @NotNull(message = "상태는 필수입니다.")
         AccountStatus status,
 
         String reason
